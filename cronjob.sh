cd1eb7b10c051f727e83039dc31b25ee8697439f
Blair <blair@fieldworker.lan>
2018-07-29 21:43:58 +1000

    [refactor] Location of folder variable

--- cronjob.sh
+++ cronjob.sh
@@ -4,12 +4,14 @@
 # PID=$(pgrep -U 1000 gnome-session)
 # export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
-
-rm -f /home/blair/gitrepos/worldmap/images/*.jpg
+worldmapdir=/home/blair/00blair/gitrepos/worldmap
+rm -f $worldmapdir/images/*.jpg
 unixtime=$(date +%s)
 xplanet -conf \
-	/home/blair/gitrepos/worldmap/config/config.conf \
+	$worldmapdir/config/config.conf \
 	-projection rectangular -geometry 1920x1080 \
-	-output /home/blair/gitrepos/worldmap/images/$unixtime.jpg --num_times 1
+	-output $worldmapdir/images/$unixtime.jpg --num_times 1
 
-gsettings set org.gnome.desktop.background picture-uri file:///home/blair/gitrepos/worldmap/images/$unixtime.jpg
+# I think this is old GNOME stuff..?
+# gsettings set org.gnome.desktop.background picture-uri file://$worldmapdir/images/$unixtime.jpg
+gsettings set org.mate.background picture-filename $worldmapdir/images/$unixtime.jpg
